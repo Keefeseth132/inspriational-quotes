31160ddb252b8e49335ba66f0960125d5a4bde62
Seth Keefer <keeferseth132@gmail.com>
2015-07-09 14:55:55 -0600

    functionality

--- inspirational-quotes.js
+++ inspirational-quotes.js
@@ -1,32 +1,79 @@
 $(document).on('ready', function(){
 
-// $('.nav-buttons').on('click', function(){
-// 	console.log('hey this button clicks')
-// })
+var quotes = [{
+	author : 'Joseph Campbell',
+	quote : 'Find a place inside where there is joy, and the joy will burn out the pain.',
+	rating : 3
+}, 
+{
+	author : 'Willie Nelson',
+	quote : 'Once you replace negative thoughts with positive ones, you will start having positive results',
+	rating : 4
+},
+{
+	author : 'William James',
+	quote : 'Believe that life is worth living and your beliefe will help create the fact',
+	rating : 5 
+}]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+var arr = []
+$('#random-quote').on('click', function(){
+	var randomQuote = _.sample(quotes)
+		arr.push(randomQuote)
+		console.log(arr)
 })
+
+
+
+$('#delete').on('click', function(){
+	console.log('delete clicked')
+})
+
+$('.submit').on('click', function(){
+	 var quoteBox = $('.text-box').val()
+	 var authorBox = $('.input-size').val() 
+	 var outPut = $('<div class="quote-container">' + quoteBox + ' - ' + authorBox + '<span class="star-color">&#9733;&#9733;&#9733;&star;&star;</span> </div>' )
+	$('.bottom-container').append(outPut)	
+	$('.text-box').val('')
+	$('.input-size').val('')
+})
+
+var sortList = function(list){
+	return _.sortBy(list, 'rating').reverse()
+}
+console.log(sortList(quotes))
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
